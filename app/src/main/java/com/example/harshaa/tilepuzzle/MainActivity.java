7a8fae92d98a0fa4efcdce4f387957b16e0d28c5
harsha <harshaa2@illinois.edu>
2018-12-05 01:32:51 -0600

    DAWG

--- app/src/main/java/com/example/harshaa/tilepuzzle/MainActivity.java
+++ app/src/main/java/com/example/harshaa/tilepuzzle/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.harshaa.tilepuzzle;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     Button b31;
     Button b32;
     Button b33;
+    Button newGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 b00.setText("1");
+                b00.setBackgroundColor(Color.RED);
             }
         });
         b01 = findViewById(R.id.b01);
@@ -42,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 b01.setText("1");
+                b01.setBackgroundColor(Color.RED);
             }
         });
         b02 = findViewById(R.id.b02);
@@ -49,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 b02.setText("1");
+                b02.setBackgroundColor(Color.RED);
             }
         });
         b03 = findViewById(R.id.b03);
@@ -56,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 b03.setText("1");
+                b03.setBackgroundColor(Color.RED);
             }
         });
         b10 = findViewById(R.id.b10);
@@ -63,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 b10.setText("1");
+                b10.setBackgroundColor(Color.RED);
             }
         });
         b11 = findViewById(R.id.b11);
@@ -70,6 +77,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 b11.setText("1");
+                b11.setBackgroundColor(Color.RED);
             }
         });
         b12 = findViewById(R.id.b12);
@@ -77,6 +85,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 b12.setText("1");
+                b12.setBackgroundColor(Color.RED);
             }
         });
         b13 = findViewById(R.id.b13);
@@ -84,6 +93,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 b13.setText("1");
+                b13.setBackgroundColor(Color.RED);
             }
         });
         b20 = findViewById(R.id.b20);
@@ -91,6 +101,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 b20.setText("1");
+                b20.setBackgroundColor(Color.RED);
             }
         });
         b21 = findViewById(R.id.b21);
@@ -98,6 +109,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 b21.setText("1");
+                b21.setBackgroundColor(Color.RED);
             }
         });
         b22 = findViewById(R.id.b22);
@@ -105,6 +117,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 b22.setText("1");
+                b22.setBackgroundColor(Color.RED);
             }
         });
         b23 = findViewById(R.id.b23);
@@ -112,6 +125,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 b23.setText("1");
+                b23.setBackgroundColor(Color.RED);
             }
         });
         b30 = findViewById(R.id.b30);
@@ -119,6 +133,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 b30.setText("1");
+                b30.setBackgroundColor(Color.RED);
             }
         });
         b31 = findViewById(R.id.b31);
@@ -126,6 +141,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 b31.setText("1");
+                b31.setBackgroundColor(Color.RED);
             }
         });
         b32 = findViewById(R.id.b32);
@@ -133,6 +149,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 b32.setText("1");
+                b32.setBackgroundColor(Color.RED);
             }
         });
         b33 = findViewById(R.id.b33);
@@ -140,9 +157,47 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 b33.setText("1");
+                b33.setBackgroundColor(Color.RED);
             }
         });
 
+        newGame = findViewById(R.id.newGame);
+        newGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                b00.setBackgroundColor(Color.GRAY);
+                b01.setBackgroundColor(Color.GRAY);
+                b02.setBackgroundColor(Color.GRAY);
+                b03.setBackgroundColor(Color.GRAY);
+                b10.setBackgroundColor(Color.GRAY);
+                b11.setBackgroundColor(Color.GRAY);
+                b12.setBackgroundColor(Color.GRAY);
+                b13.setBackgroundColor(Color.GRAY);
+                b20.setBackgroundColor(Color.GRAY);
+                b21.setBackgroundColor(Color.GRAY);
+                b22.setBackgroundColor(Color.GRAY);
+                b23.setBackgroundColor(Color.GRAY);
+                b30.setBackgroundColor(Color.GRAY);
+                b31.setBackgroundColor(Color.GRAY);
+                b32.setBackgroundColor(Color.GRAY);
+                b33.setBackgroundColor(Color.GRAY);
+                b00.setText("0");
+                b01.setText("0");
+                b02.setText("0");
+                b03.setText("0");
+                b10.setText("0");
+                b11.setText("0");
+                b12.setText("0");
+                b13.setText("0");
+                b20.setText("0");
+                b21.setText("0");
+                b22.setText("0");
+                b23.setText("0");
+                b30.setText("0");
+                b31.setText("0");
+                b32.setText("0");
+                b33.setText("0");
+            }
+        });
     }
-
 }
