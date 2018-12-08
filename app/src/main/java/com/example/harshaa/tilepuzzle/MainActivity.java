a7b0488503b5b2b14045771302b138b2a3c8ac5c
harsha <harshaa2@illinois.edu>
2018-12-08 17:35:36 -0600

    brooo

--- app/src/main/java/com/example/harshaa/tilepuzzle/MainActivity.java
+++ app/src/main/java/com/example/harshaa/tilepuzzle/MainActivity.java
@@ -36,286 +36,313 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*b[0][0] = findViewById(R.id.b00);
-        b[0][0].setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                blackSpot = b[thisGame.getBlackPieceX()][thisGame.getBlackPieceY()];
-                if (thisGame.switchTile(0,0)) {
-                    blackSpot.setText((char) thisGame.getBoardAt(0,0));
-                    blackSpot.setBackgroundColor(Color.WHITE);
-                    b[0][0].setText("0");
-                    b[0][0].setBackgroundColor(Color.RED);
-                    blackSpot = b[0][0];
-                }
-
-            }
-        });*/
         b[0][0] = findViewById(R.id.b00);
+        b[0][1] = findViewById(R.id.b01);
+        b[0][2] = findViewById(R.id.b02);
+        b[0][3] = findViewById(R.id.b03);
+        b[1][0] = findViewById(R.id.b10);
+        b[1][1] = findViewById(R.id.b11);
+        b[1][2] = findViewById(R.id.b12);
+        b[1][3] = findViewById(R.id.b13);
+        b[2][0] = findViewById(R.id.b20);
+        b[2][1] = findViewById(R.id.b21);
+        b[2][2] = findViewById(R.id.b22);
+        b[2][3] = findViewById(R.id.b23);
+        b[3][0] = findViewById(R.id.b30);
+        b[3][1] = findViewById(R.id.b31);
+        b[3][2] = findViewById(R.id.b32);
+        b[3][3] = findViewById(R.id.b33);
+
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                b[i][j].setText(Integer.toString(thisGame.getBoardAt(i, j)));
+                b[i][j].setBackgroundColor(Color.WHITE);
+                b[i][j].setTextSize(25);
+            }
+        }
+        b[3][3].setText(Integer.toString(0));
+        b[3][3].setBackgroundColor(Color.BLACK);
+        b[3][3].setTextColor(Color.WHITE);
+
         b[0][0].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (thisGame.canSwitchTile(0,0)) {
                     b[0][0].setText("0");
-                    b[0][0].setBackgroundColor(Color.RED);
+                    b[0][0].setTextColor(Color.WHITE);
+                    b[0][0].setBackgroundColor(Color.BLACK);
                     int bx = thisGame.getBlackPieceX();
                     int by = thisGame.getBlackPieceY();
                     b[bx][by].setBackgroundColor(Color.WHITE);
-                    b[bx][by].setText(Integer.toString(3));
+                    b[bx][by].setTextColor(Color.BLACK);
                     b[bx][by].setText(Integer.toString(thisGame.switchTile(0, 0)));
+                    if (thisGame.check()) {
+                        
+                    }
                 }
 
             }
         });
 
-        b[0][1] = findViewById(R.id.b01);
+
         b[0][1].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (thisGame.canSwitchTile(0,1)) {
                     b[0][1].setText("0");
-                    b[0][1].setBackgroundColor(Color.RED);
+                    b[0][1].setTextColor(Color.WHITE);
+                    b[0][1].setBackgroundColor(Color.BLACK);
                     int bx = thisGame.getBlackPieceX();
                     int by = thisGame.getBlackPieceY();
                     b[bx][by].setBackgroundColor(Color.WHITE);
-                    b[bx][by].setText(Integer.toString(3));
+                    b[bx][by].setTextColor(Color.BLACK);
                     b[bx][by].setText(Integer.toString(thisGame.switchTile(0, 1)));
                 }
 
             }
         });
 
-        b[0][2] = findViewById(R.id.b02);
+
         b[0][2].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (thisGame.canSwitchTile(0,2)) {
                     b[0][2].setText("0");
-                    b[0][2].setBackgroundColor(Color.RED);
+                    b[0][2].setTextColor(Color.WHITE);
+                    b[0][2].setBackgroundColor(Color.BLACK);
                     int bx = thisGame.getBlackPieceX();
                     int by = thisGame.getBlackPieceY();
                     b[bx][by].setBackgroundColor(Color.WHITE);
-                    b[bx][by].setText(Integer.toString(3));
+                    b[bx][by].setTextColor(Color.BLACK);
                     b[bx][by].setText(Integer.toString(thisGame.switchTile(0, 2)));
                 }
 
             }
         });
 
-        b[0][3] = findViewById(R.id.b03);
+
         b[0][3].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (thisGame.canSwitchTile(0,3)) {
                     b[0][3].setText("0");
-                    b[0][3].setBackgroundColor(Color.RED);
+                    b[0][3].setTextColor(Color.WHITE);
+                    b[0][3].setBackgroundColor(Color.BLACK);
                     int bx = thisGame.getBlackPieceX();
                     int by = thisGame.getBlackPieceY();
                     b[bx][by].setBackgroundColor(Color.WHITE);
-                    b[bx][by].setText(Integer.toString(3));
+                    b[bx][by].setTextColor(Color.BLACK);
                     b[bx][by].setText(Integer.toString(thisGame.switchTile(0, 3)));
                 }
 
             }
         });
 
-        b[1][0] = findViewById(R.id.b10);
         b[1][0].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (thisGame.canSwitchTile(1,0)) {
                     b[1][0].setText("0");
-                    b[1][0].setBackgroundColor(Color.RED);
+                    b[1][0].setTextColor(Color.WHITE);
+                    b[1][0].setBackgroundColor(Color.BLACK);
                     int bx = thisGame.getBlackPieceX();
                     int by = thisGame.getBlackPieceY();
                     b[bx][by].setBackgroundColor(Color.WHITE);
-                    b[bx][by].setText(Integer.toString(3));
+                    b[bx][by].setTextColor(Color.BLACK);
                     b[bx][by].setText(Integer.toString(thisGame.switchTile(1, 0)));
                 }
 
             }
         });
 
-        b[1][1] = findViewById(R.id.b11);
         b[1][1].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (thisGame.canSwitchTile(1,1)) {
                     b[1][1].setText("0");
-                    b[1][1].setBackgroundColor(Color.RED);
+                    b[1][1].setTextColor(Color.WHITE);
+                    b[1][1].setBackgroundColor(Color.BLACK);
                     int bx = thisGame.getBlackPieceX();
                     int by = thisGame.getBlackPieceY();
                     b[bx][by].setBackgroundColor(Color.WHITE);
-                    b[bx][by].setText(Integer.toString(3));
+                    b[bx][by].setTextColor(Color.BLACK);
                     b[bx][by].setText(Integer.toString(thisGame.switchTile(1, 1)));
                 }
 
             }
         });
 
-        b[1][2] = findViewById(R.id.b12);
+
         b[1][2].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (thisGame.canSwitchTile(1,2)) {
                     b[1][2].setText("0");
-                    b[1][2].setBackgroundColor(Color.RED);
+                    b[1][2].setTextColor(Color.WHITE);
+                    b[1][2].setBackgroundColor(Color.BLACK);
                     int bx = thisGame.getBlackPieceX();
                     int by = thisGame.getBlackPieceY();
                     b[bx][by].setBackgroundColor(Color.WHITE);
-                    b[bx][by].setText(Integer.toString(3));
+                    b[bx][by].setTextColor(Color.BLACK);
                     b[bx][by].setText(Integer.toString(thisGame.switchTile(1, 2)));
                 }
 
             }
         });
 
-        b[1][3] = findViewById(R.id.b13);
         b[1][3].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (thisGame.canSwitchTile(1,3)) {
                     b[1][3].setText("0");
-                    b[1][3].setBackgroundColor(Color.RED);
+                    b[1][3].setTextColor(Color.WHITE);
+                    b[1][3].setBackgroundColor(Color.BLACK);
                     int bx = thisGame.getBlackPieceX();
                     int by = thisGame.getBlackPieceY();
                     b[bx][by].setBackgroundColor(Color.WHITE);
-                    b[bx][by].setText(Integer.toString(3));
+                    b[bx][by].setTextColor(Color.BLACK);
                     b[bx][by].setText(Integer.toString(thisGame.switchTile(1, 3)));
                 }
 
             }
         });
 
-        b[2][0] = findViewById(R.id.b20);
+
         b[2][0].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (thisGame.canSwitchTile(2,0)) {
                     b[2][0].setText("0");
-                    b[2][0].setBackgroundColor(Color.RED);
+                    b[2][0].setTextColor(Color.WHITE);
+                    b[2][0].setBackgroundColor(Color.BLACK);
                     int bx = thisGame.getBlackPieceX();
                     int by = thisGame.getBlackPieceY();
                     b[bx][by].setBackgroundColor(Color.WHITE);
-                    b[bx][by].setText(Integer.toString(3));
+                    b[bx][by].setTextColor(Color.BLACK);
                     b[bx][by].setText(Integer.toString(thisGame.switchTile(2, 0)));
                 }
 
             }
         });
 
-        b[2][1] = findViewById(R.id.b21);
+
         b[2][1].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (thisGame.canSwitchTile(2,1)) {
                     b[2][1].setText("0");
-                    b[2][1].setBackgroundColor(Color.RED);
+                    b[2][1].setTextColor(Color.WHITE);
+                    b[2][1].setBackgroundColor(Color.BLACK);
                     int bx = thisGame.getBlackPieceX();
                     int by = thisGame.getBlackPieceY();
                     b[bx][by].setBackgroundColor(Color.WHITE);
-                    b[bx][by].setText(Integer.toString(3));
+                    b[bx][by].setTextColor(Color.BLACK);
                     b[bx][by].setText(Integer.toString(thisGame.switchTile(2, 1)));
                 }
 
             }
         });
 
-        b[2][2] = findViewById(R.id.b22);
+
         b[2][2].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (thisGame.canSwitchTile(2,2)) {
                     b[2][2].setText("0");
-                    b[2][2].setBackgroundColor(Color.RED);
+                    b[2][2].setTextColor(Color.WHITE);
+                    b[2][2].setBackgroundColor(Color.BLACK);
                     int bx = thisGame.getBlackPieceX();
                     int by = thisGame.getBlackPieceY();
                     b[bx][by].setBackgroundColor(Color.WHITE);
-                    b[bx][by].setText(Integer.toString(3));
+                    b[bx][by].setTextColor(Color.BLACK);
                     b[bx][by].setText(Integer.toString(thisGame.switchTile(2, 2)));
                 }
 
             }
         });
 
-        b[2][3] = findViewById(R.id.b23);
+
         b[2][3].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (thisGame.canSwitchTile(2,3)) {
                     b[2][3].setText("0");
-                    b[2][3].setBackgroundColor(Color.RED);
+                    b[2][3].setTextColor(Color.WHITE);
+                    b[2][3].setBackgroundColor(Color.BLACK);
                     int bx = thisGame.getBlackPieceX();
                     int by = thisGame.getBlackPieceY();
                     b[bx][by].setBackgroundColor(Color.WHITE);
-                    b[bx][by].setText(Integer.toString(3));
+                    b[bx][by].setTextColor(Color.BLACK);
                     b[bx][by].setText(Integer.toString(thisGame.switchTile(2, 3)));
                 }
 
             }
         });
 
-        b[3][0] = findViewById(R.id.b30);
         b[3][0].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (thisGame.canSwitchTile(3,0)) {
                     b[3][0].setText("0");
-                    b[3][0].setBackgroundColor(Color.RED);
+                    b[3][0].setTextColor(Color.WHITE);
+                    b[3][0].setBackgroundColor(Color.BLACK);
                     int bx = thisGame.getBlackPieceX();
                     int by = thisGame.getBlackPieceY();
                     b[bx][by].setBackgroundColor(Color.WHITE);
-                    b[bx][by].setText(Integer.toString(3));
+                    b[bx][by].setTextColor(Color.BLACK);
                     b[bx][by].setText(Integer.toString(thisGame.switchTile(3, 0)));
                 }
 
             }
         });
 
-        b[3][1] = findViewById(R.id.b31);
+
         b[3][1].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (thisGame.canSwitchTile(3,1)) {
                     b[3][1].setText("0");
-                    b[3][1].setBackgroundColor(Color.RED);
+                    b[3][1].setTextColor(Color.WHITE);
+                    b[3][1].setBackgroundColor(Color.BLACK);
                     int bx = thisGame.getBlackPieceX();
                     int by = thisGame.getBlackPieceY();
                     b[bx][by].setBackgroundColor(Color.WHITE);
-                    b[bx][by].setText(Integer.toString(3));
+                    b[bx][by].setTextColor(Color.BLACK);
                     b[bx][by].setText(Integer.toString(thisGame.switchTile(3, 1)));
                 }
 
             }
         });
 
-        b[3][2] = findViewById(R.id.b32);
         b[3][2].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (thisGame.canSwitchTile(3,2)) {
                     b[3][2].setText("0");
-                    b[3][2].setBackgroundColor(Color.RED);
+                    b[3][2].setTextColor(Color.WHITE);
+                    b[3][2].setBackgroundColor(Color.BLACK);
                     int bx = thisGame.getBlackPieceX();
                     int by = thisGame.getBlackPieceY();
                     b[bx][by].setBackgroundColor(Color.WHITE);
-                    b[bx][by].setText(Integer.toString(3));
+                    b[bx][by].setTextColor(Color.BLACK);
                     b[bx][by].setText(Integer.toString(thisGame.switchTile(3, 2)));
                 }
 
             }
         });
-        b[3][3] = findViewById(R.id.b33);
+
         b[3][3].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (thisGame.canSwitchTile(3,3)) {
                     b[3][3].setText("0");
-                    b[3][3].setBackgroundColor(Color.RED);
+                    b[3][3].setTextColor(Color.WHITE);
+                    b[3][3].setBackgroundColor(Color.BLACK);
                     int bx = thisGame.getBlackPieceX();
                     int by = thisGame.getBlackPieceY();
                     b[bx][by].setBackgroundColor(Color.WHITE);
-                    b[bx][by].setText(Integer.toString(3));
+                    b[bx][by].setTextColor(Color.BLACK);
                     b[bx][by].setText(Integer.toString(thisGame.switchTile(3, 3)));
                 }
 
@@ -481,5 +508,7 @@ public class MainActivity extends AppCompatActivity {
                 b33.setText("0");
             }
         });*/
+
+
     }
 }
--- app/src/main/java/com/example/harshaa/tilepuzzle/TilePuzzle.java
+++ app/src/main/java/com/example/harshaa/tilepuzzle/TilePuzzle.java
@@ -1,11 +1,21 @@
 package com.example.harshaa.tilepuzzle;
+
+import java.util.Random;
+
 public class TilePuzzle {
     public final int len = 4;
     private int[][] board = new int[len][len];
     private int[] blackPiece = {len - 1, len - 1};
+    private int numbertile = 15;
+    int[] tiles = new int[16];
+    private Random myst = new Random();
 
     public TilePuzzle() {
         initializeBoard();
+        shuffleBoard();
+        while (!isValid()) {
+            shuffleBoard();
+        }
 
     }
 
@@ -14,6 +24,7 @@ public class TilePuzzle {
         for (int i = 0; i < len; i++) {
             for (int j = 0; j < len; j++) {
                 board[i][j] = count;
+                tiles[count - 1] = count;
                 count++;
             }
         }
@@ -30,6 +41,7 @@ public class TilePuzzle {
         return true;
 
     }
+
     public int switchTile(int x, int y) {
         int trans = board[x][y];
         board[x][y] = 0;
@@ -38,16 +50,55 @@ public class TilePuzzle {
         blackPiece[1] = y;
         return trans;
     }
+
     public int getBoardAt(int x, int y) {
         return board[x][y];
     }
+
     public int getBlackPieceX() {
         return blackPiece[0];
     }
+
     public int getBlackPieceY() {
         return blackPiece[1];
     }
-    /*public void shuffleBoard(final int iterations) {
-        for (int i = 0; i < iterations; i++) {
-            if () {*/
+
+    public void shuffleBoard() {
+        for (int i = 0; i < numbertile; i++) {
+            tiles[i] = i + 1;
+        }
+        int a = tiles.length - 1;
+        while (a > 1) {
+            int s = myst.nextInt(a--);
+            int temp = tiles[s];
+            tiles[s] = tiles[a];
+            tiles[a] = temp;
+        }
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                board[i][j] = tiles[(len * i) + j];
+            }
+        }
+    }
+    public boolean check() {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] != (i * len) + j) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public boolean isValid() {
+        int numInversions = 0;
+        for (int i = 0; i < tiles.length - 1; i++) {
+            for (int j = 0; j < i; j++) {
+                if (tiles[j] > tiles[i]) {
+                    numInversions++;
+                }
+            }
+        }
+        return numInversions % 2 == 0;
+    }
 }
